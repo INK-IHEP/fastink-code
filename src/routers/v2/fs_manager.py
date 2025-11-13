4c56b10f45f93280865f4bc136caaf1edc374878
Yujiang Bi <biyujiang@ihep.ac.cn>
2025-11-13 09:36:43 +0800

    fix fs rename

--- src/routers/v2/fs_manager.py
+++ src/routers/v2/fs_manager.py
@@ -362,8 +362,8 @@ async def chmod(req: Request, username:str = Depends(get_username)):
         else:
             return {"status": InkStatus.FS_UNKNOWN_ERROR, "msg": f"Failed to rename {src_name} to {dst_name}.", "data": None}
     except Exception as e:
-        logger.error(f"Failed to rename {src_name} to {dst_name}. Err: {str(e)}.")
-        return {"status": InkStatus.FS_UNKNOWN_ERROR, "msg": f"Failed to rename {src_name} to {dst_name}. Err: {str(e)}.", "data": None}
+        logger.error(f"Failed to perform rename operation. Err: {str(e)}.")
+        return {"status": InkStatus.FS_UNKNOWN_ERROR, "msg": f"Failed to perform rename operation. Err: {str(e)}.", "data": None}
 
 if __name__ == "__main__":
 
