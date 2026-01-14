d982abc1d0136516ec9cbb3028c727285d0d0ca4
guocq <guocq@ihep.ac.cn>
2026-01-14 16:40:49 +0800

    测试提交的问题，很头疼

--- src/computing/crond/update_job_status.py
+++ src/computing/crond/update_job_status.py
@@ -300,7 +300,7 @@ async def submit_job_from_redis():
                 except Exception as e:
                     if job_owner:
                         await r.lrem(f"{job_owner}_submitting_jobs", 1, raw_job)
-                    logger.exception(f"HTC-LOG: Submit {raw_job} job failed, {e}")
+                    logger.exception(f"HTC-LOG: Submit job failed, {e}")
                     continue
 
     except Timeout:
