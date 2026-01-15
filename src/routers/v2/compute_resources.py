8165b74ce9eacb9c149df1d17f3b22778e0a9297
guocq <guocq@ihep.ac.cn>
2026-01-15 15:40:43 +0800

    更新了删除方法的返回值

--- src/routers/v2/compute_resources.py
+++ src/routers/v2/compute_resources.py
@@ -380,11 +380,7 @@ async def delete_common_job(
 ):
     try:
         if not job_id:
-            return {
-                "status": InkStatus.PARAM_ERROR,
-                "msg": f"",
-                "data": {}
-            }
+            return 
 
         adapter = get_scheduler(cluster_id, username)
         await adapter.cancel_job(job_id)
