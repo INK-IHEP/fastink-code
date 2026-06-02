352361c75c784a7609d90cd0966e5fcb1a36904e
Zhaoyi Qu <liangququ@mail.nankai.edu.cn>
2026-06-02 23:49:56 +0800

    Fix:  correct the running procedure of shell.sh and run.sh Feat: add new feature to the display mode, now html rendering and WebSocket is running in the same port, now the display algorithm needs one port only.

--- src/fastink/computing/scripts/herd_display/run.sh
+++ src/fastink/computing/scripts/herd_display/run.sh
@@ -1,36 +1,12 @@
 #!/bin/bash
 
+APP_PORT=${1}
+HERD_ROOT=${2}
+HERD_XML=${3}
+HERD_BIN=${4}
+APP_LOGIN_INFO="herddisplay_login.info"
+APP_RUN_HOST="`/bin/hostname | /bin/awk -F '.' '{print $1}'`"
 
-if [ -n "${INKPATH:-}" ] && [ -n "${INKLDPATH:-}" ]; then
-    export PATH="$INKPATH:$PATH"
-    export APPTAINERENV_PATH="$INKPATH"
-    export LD_LIBRARY_PATH=$INKLDPATH
-    export APPTAINERENV_LD_LIBRARY_PATH=$INKLDPATH
-fi
+/bin/echo "{\"HOST\": \"${APP_RUN_HOST}\", \"PORT\": \"${APP_PORT}\", \"xml\": \"${HERD_XML}\", \"root\": \"${HERD_ROOT}\"}" > ${APP_LOGIN_INFO}
 
-function get_free_port() {
-    while true; do
-        PORT=$(shuf -i 49152-65535 -n 1)
-        if ! /usr/sbin/ss -ltn | grep -q ":$PORT\b"; then
-            echo $PORT
-            break
-        fi
-    done
-}
-
-# Configure HERD Event Display environment
-APP_PORT=$(get_free_port)
-APP_PATH="`/bin/pwd`"
-if [ -f "${APP_PATH}/krb5cc_${UID}" ]; then
-  export KRB5CCNAME="${APP_PATH}/krb5cc_${UID}"
-fi
-
-if command -v /usr/bin/aklog >/dev/null 2>&1 && klist -s 2>/dev/null; then
-  /usr/bin/aklog
-fi
-
-export HERD_BIN="/herdfs/user/quzy/public/runink.sh"
-export HERD_XML="compact/v2024b/v2024b-test.xml"
-export HERD_ROOT="/herdfs/user/quzy/public/demo/compact_electron_30.root"
-
-${APP_PATH}/run.sh ${APP_PORT}  ${HERD_XML}  ${HERD_ROOT}
+/bin/bash ${HERD_BIN} ${APP_PORT} ${HERD_XML} ${HERD_ROOT} 2>&1
