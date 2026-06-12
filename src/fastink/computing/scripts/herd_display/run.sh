ca8759775ff600e5d6ac0857643d2399445c092b
qu zhao yi <quzy@lxlogin004.ihep.ac.cn>
2026-06-12 14:20:30 +0800

    fix: remove unnecessary info in the login.info file

--- src/fastink/computing/scripts/herd_display/run.sh
+++ src/fastink/computing/scripts/herd_display/run.sh
@@ -7,6 +7,6 @@ HERD_BIN=${4}
 APP_LOGIN_INFO="app_login.info"
 APP_RUN_HOST="`/bin/hostname | /bin/awk -F '.' '{print $1}'`"
 
-/bin/echo "{\"HOST\": \"${APP_RUN_HOST}\", \"PORT\": \"${APP_PORT}\", \"xml\": \"${HERD_XML}\", \"root\": \"${HERD_ROOT}\"}" > ${APP_LOGIN_INFO}
+/bin/echo "{\"HOST\": \"${APP_RUN_HOST}\", \"PORT\": \"${APP_PORT}\", \"TOKEN\": \"\"  }" > ${APP_LOGIN_INFO}
 
 /bin/bash ${HERD_BIN} ${APP_PORT} ${HERD_XML} ${HERD_ROOT} ${APP_RUN_HOST} 2>&1
