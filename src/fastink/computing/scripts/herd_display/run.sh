461ff5a4894d5e4e9b26f3991e7c0300ad75eb1b
qu zhao yi <quzy@lxlogin004.ihep.ac.cn>
2026-06-12 13:54:26 +0800

    feat: add support for users outside IHEP

--- src/fastink/computing/scripts/herd_display/run.sh
+++ src/fastink/computing/scripts/herd_display/run.sh
@@ -9,4 +9,4 @@ APP_RUN_HOST="`/bin/hostname | /bin/awk -F '.' '{print $1}'`"
 
 /bin/echo "{\"HOST\": \"${APP_RUN_HOST}\", \"PORT\": \"${APP_PORT}\", \"xml\": \"${HERD_XML}\", \"root\": \"${HERD_ROOT}\"}" > ${APP_LOGIN_INFO}
 
-/bin/bash ${HERD_BIN} ${APP_PORT} ${HERD_XML} ${HERD_ROOT} 2>&1
+/bin/bash ${HERD_BIN} ${APP_PORT} ${HERD_XML} ${HERD_ROOT} ${APP_RUN_HOST} 2>&1
